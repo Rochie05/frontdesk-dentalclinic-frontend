fcb2d33d90f9f8612b4c61f957c97bcd9e29ef7f
Rochie <cabatlao.rochelyn.perez@gmail.com>
2025-10-04 21:39:21 +0800

    Com

---
+++ src/pages/Receptionist.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '@/components/dashboard/Sidebar';
+import ReceptionistHeader from '@/components/receptionist/ReceptionistHeader';
+import ReceptionistTable from '@/components/receptionist/ReceptionistTable';
+
+export default function Receptionist() {
+  return (<>
+    <Sidebar/>
+    <ReceptionistHeader/>
+    <ReceptionistTable/>
+    </>
+  )
+}
